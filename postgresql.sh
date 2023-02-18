d32790760a7c0ea2b58265c2079ebab328e6abaf
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2023-02-18 09:35:20 -0300

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# debian@debian:~$ sudo -u postgres psql postgres
+
+# or
+
 # debian@debian:~$ sudo su - postgres
 # postgres@debian:~$ psql
 # postgres=# ALTER USER postgres WITH PASSWORD 'password';
