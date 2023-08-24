b5f04538a76e12f71d5bb18a6711485b66842ae8
Leandro Sciola <leandrosciola@gmail.com>
2023-08-24 09:57:05 -0300

    Add

---
+++ git.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#git clone https://<token>@github.com/Leandro-Sciola/<repo>.git
+#git remote set-url origin https://<token>@github.com/Leandro-Sciola/<repo>.git
+
+git add .
+git commit -m "Updated"
+git push origin main
+git status
