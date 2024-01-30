cf9ab196ba1ed88bd932cf3ccbb9c4a84f217737
GitHub <github.repo@outlook.com>
2024-01-30 13:03:43 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#git clone https://<token>@github.com/Leandro-Sciola/<repo>.git
-#git remote set-url origin https://<token>@github.com/Leandro-Sciola/<repo>.git
+#git clone https://<token>@github.com/apps-online/<repo>.git
+#git remote set-url origin https://<token>@github.com/apps-online/<repo>.git
 
 git add .
 git commit -m "Updated"
